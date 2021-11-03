6714d8fa9478ef5310b0717c29ac700fa6ef50b7
stayhigh <redhero0702@gmail.com>
2021-11-04 02:13:57 +0800

    add outdir, download, nresult, 3 options totally

--- arxiv_search.py
+++ arxiv_search.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+import os.path
 
 import arxiv
 import argparse
 import logging
 import csv
+import sys
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,8 +15,10 @@ parser = argparse.ArgumentParser(description='Arguments')
 parser.add_argument('--kw', type=str, help="""Keyword to be searched. Use double quote followed by simple quote to search for an exact keyword. Example: "'exact keyword'" """)
 parser.add_argument('--sort_order', default=arxiv.SortOrder.Descending, type=type(arxiv.SortOrder.Descending), help='Column to be sorted by. Default is by the columns "Citations", i.e., it will be sorted by the number of citations. If you want to sort by citations per year, use --sortby "cit/year"')
 parser.add_argument('--sort_by', default=arxiv.SortCriterion.SubmittedDate, type=type(arxiv.SortCriterion.SubmittedDate), help='sort by')
-parser.add_argument('--outdir', default='./papers', type=str, help='output dir')
-#parser.add_argument('--nresults', type=int, help='Number of articles to search on Google Scholar. Default is 100. (carefull with robot checking if value is too high)')
+parser.add_argument('--outdir', default=os.path.join(os.path.dirname(__file__), './papers'), type=str, help='output dir')
+parser.add_argument('--download', action="store_true", help='enable download if True')
+parser.add_argument('--nresults', default=10, type=int, help='Number of articles to search on arxiv. Default is 10. (carefull with robot checking if value is too high)')
+
 #parser.add_argument('--csvpath', type=str, help='Path to save the exported csv file. By default it is the current folder')
 #parser.add_argument('--notsavecsv', action='store_true', help='By default results are going to be exported to a csv file. Select this option to just print results but not store them')
 #parser.add_argument('--plotresults', action='store_true', help='Use this flag in order to plot the results with the original rank in the x-axis and the number of citaions in the y-axis. Default is False')
@@ -27,7 +31,7 @@ args, _ = parser.parse_known_args()
 
 search = arxiv.Search(
   query=args.kw,
-  max_results=3,
+  max_results=args.nresults,
   sort_by=args.sort_by,       # arxiv.SortCriterion.SubmittedDate,
   sort_order=args.sort_order  # sort_order = arxiv.SortOrder.Descending
 )
@@ -76,22 +80,37 @@ def show_all_arxiv_structre(searchObj):
 
 def show_breif_arxiv_structre(searchObj):
   result = [
-    str(searchObj.published),
-    searchObj.title,
     "/".join(map(str, searchObj.authors)),
+    searchObj.title,
+    str(searchObj.published),
+    searchObj.pdf_url,
     searchObj.summary.replace("\n", ' '),
-    searchObj.pdf_url
   ]
-
   return result
 
 
 if __name__ == '__main__':
-    with open(f"{args.kw}.csv", "w") as f:
+    filename = f"{args.kw}.csv"
+    write_header = False
+    header = ['author', 'title', 'publish_date', 'pdf_url', 'summary']
+
+    if not os.path.exists(args.outdir):
+        os.mkdir(args.outdir)
+
+    with open(filename, "w") as f:
         for result in client.results(search):
             st = show_breif_arxiv_structre(result)
-            writer = csv.writer(f, delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
-            writer.writerow(st)
+            try:
+                writer = csv.writer(f, delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
+                if not write_header:
+                    writer.writerow(header)
+                    write_header = True
+                writer.writerow(st)
+            except csv.Error as e:
+                sys.exit(f'file {filename},  {e}')
+
+            if args.download:
+                result.download_pdf(dirpath=args.outdir, filename=f"{result.title}.pdf")
 
 
 
